26f545f59ffe51577e8e0295d39f4a36c18ed519
kimbrow1 <you@example.com>
2024-03-04 16:38:22 -0600

    script

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ const weapons = [
   { name: 'claw hammer', power: 50 },
   { name: 'sword', power: 100 }
 ];
+
 const locations = [
   {
     name: "town square",
@@ -70,10 +71,6 @@ function goCave() {
   update(locations[2]);
 }
 
-function fightDragon() {
-  console.log("Fighting dragon.");
-}
-
 function buyHealth() {
   if (gold >= 10) {
     gold -= 10;
@@ -100,6 +97,20 @@ function buyWeapon() {
     }
   } else {
     text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
+}
+
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
   }
 }
 
@@ -109,4 +120,8 @@ function fightSlime() {
 
 function fightBeast() {
 
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.");
 }
