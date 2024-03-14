3d6de8097aeb2b563cad397a594f7d52bfaac9e1
kimbrow1 <you@example.com>
2024-03-14 17:25:35 -0500

    script

--- script.js
+++ script.js
@@ -57,6 +57,12 @@ const locations = [
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
   }
 ];
 
@@ -146,13 +152,15 @@ function fightDragon() {
 }
 
 function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display ='block';
+}
+
+function attack() {
 
 }
 
-function attack () {
+function dodge() {
 
-}
-
-function dodge () {
-  
 }
